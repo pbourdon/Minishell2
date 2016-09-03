27778cde2adbfa24216d840ee6a7bd9728b883c6
Paul BOURDON <pbourdon@e2r8p14.42.fr>
2016-09-03 13:50:44 +0200

    last push

--- ft_run_echo.c
+++ ft_run_echo.c
@@ -6,7 +6,7 @@
 /*   By: pbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/18 23:18:40 by pbourdon          #+#    #+#             */
-/*   Updated: 2016/08/29 20:21:25 by pbourdon         ###   ########.fr       */
+/*   Updated: 2016/09/03 13:43:14 by pbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void		ft_echo_var_env(int index, char *arg, t_dlist *list)
 	char		*tmp;
 
 	tmp = ft_strjoin(arg + index + 1, "=");
-	if (arg[index] == '$' && ft_search_list(list, ft_strupcase(tmp)) != 0)
+	if (arg[index] == '$' && ft_search_list(list, tmp) != 0)
 	{
-		ft_putstr(get_ele(list, ft_search_list(list, ft_strupcase(tmp))) +
+		ft_putstr(get_ele(list, ft_search_list(list, tmp)) +
 			ft_strlen(arg + index));
 		ft_putchar('\n');
 	}
@@ -36,7 +36,7 @@ void		ft_run_echo(char *arg, t_dlist *list)
 		index++;
 	if (arg[index] == '$')
 		ft_echo_var_env(index, arg, list);
-	if (arg[index] == '-' && arg[index + 1] == 'n')
+	else if (arg[index] == '-' && arg[index + 1] == 'n')
 	{
 		index = index + 2;
 		while (arg[index] == '\t' || arg[index] == '\n' || arg[index] == '\r'
